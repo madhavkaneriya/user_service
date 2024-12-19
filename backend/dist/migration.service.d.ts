230325c523067059579277dfba411064f00e4393
madhavkaneriya <madhav.p.kaneriya@gmail.com>
2024-12-19 14:48:07 +0530

    removed accidental dist commit, added to gitignore

--- backend/dist/migration.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { Connection } from 'mongoose';
-export declare class MigrationService implements OnModuleInit {
-    private readonly connection;
-    constructor(connection: Connection);
-    onModuleInit(): Promise<void>;
-    private runMigration;
-    private collectionExists;
-}
